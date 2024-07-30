821d113344fcd06a4ac408fae64d91085116f2b6
Soheil Khaki <soheilk81@gmail.com>
2024-07-30 14:16:06 +0330

    modified:   daftarMain/__pycache__/models.cpython-311.pyc 	modified:   daftarMain/models.py 	modified:   daftarMain/__pycache__/models.cpython-311.pyc

--- daftarMain/models.py
+++ daftarMain/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    last_update = models.DateTimeField(auto_now=True)
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=255)
+    birth_date = models.DateField(null=True)
+
+    
