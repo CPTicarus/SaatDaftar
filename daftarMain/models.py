ecabd663ac12a513825ea3114e0bbdc78b6759b0
Soheil Khaki <soheilk81@gmail.com>
2024-08-01 08:41:20 +0330

    modified:   daftarMain/__pycache__/models.cpython-311.pyc 	modified:   daftarMain/models.py

--- daftarMain/models.py
+++ daftarMain/models.py
@@ -1,16 +1,25 @@
 from django.db import models
 
-class Product(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    last_update = models.DateTimeField(auto_now=True)
-
-
-class User(models.Model):
+class OfficeUser(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    email = models.EmailField(unique=True)
-    phone = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255,unique=True)
+    home_phone = models.CharField(max_length=10,unique=True,null=True)
     birth_date = models.DateField(null=True)
+    code_meli = models.CharField(max_length=10)
 
+    
+class OffceAdmin(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255,unique=True)
+    birth_date = models.DateField(null=True)
+    code_meli = models.CharField(max_length=10)
+
+
+class  Clock(models.Model):
+    entry_to_office = models.DateTimeField(auto_now=True)
+    exit_from_office = models.DateTimeField(auto_now=True)
+    wait_time_start = models.DateTimeField(auto_now=True)
+    wait_time_finish = models.DateTimeField(auto_now=True)
     
