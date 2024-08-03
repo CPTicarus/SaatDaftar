f122f796b9899527da31e90f2261db3e7ae35d6d
Soheil Khaki <soheilk81@gmail.com>
2024-08-03 09:07:52 +0330

    new file:   daftarMain/__pycache__/forms.cpython-311.pyc 	modified:   daftarMain/__pycache__/models.cpython-311.pyc 	modified:   daftarMain/__pycache__/urls.cpython-311.pyc 	modified:   daftarMain/__pycache__/views.cpython-311.pyc 	new file:   daftarMain/forms.py 	new file:   daftarMain/migrations/0001_initial.py 	new file:   daftarMain/migrations/__pycache__/0001_initial.cpython-311.pyc 	modified:   daftarMain/models.py 	modified:   daftarMain/urls.py 	modified:   daftarMain/views.py 	modified:   db.sqlite3

--- daftarMain/models.py
+++ daftarMain/models.py
@@ -1,20 +1,21 @@
 from django.db import models
 
+class OfficeManager(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=15,unique=True)
+    birth_date = models.DateField(null=True)
+    code_meli = models.CharField(max_length=10,null=True)
+
+
 class OfficeUser(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    phone = models.CharField(max_length=255,unique=True)
-    home_phone = models.CharField(max_length=10,unique=True,null=True)
+    phone = models.CharField(max_length=15,unique=True)
+    home_phone = models.CharField(max_length=15,unique=True,null=True)
     birth_date = models.DateField(null=True)
     code_meli = models.CharField(max_length=10)
-
-    
-class OffceAdmin(models.Model):
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    phone = models.CharField(max_length=255,unique=True)
-    birth_date = models.DateField(null=True)
-    code_meli = models.CharField(max_length=10)
+    office_admin = models.ForeignKey(OfficeManager,on_delete=models.CASCADE)
 
 
 class  Clock(models.Model):
@@ -22,4 +23,9 @@ class  Clock(models.Model):
     exit_from_office = models.DateTimeField(auto_now=True)
     wait_time_start = models.DateTimeField(auto_now=True)
     wait_time_finish = models.DateTimeField(auto_now=True)
+    office_user = models.ForeignKey( OfficeUser ,on_delete=models.CASCADE)
+
+
+class Sallary(models.Model):
+    monthly = models.IntegerField(max_length=12,null=True)
     
--- daftarMain/views.py
+++ daftarMain/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import RegisterForm
 
 
-def say_hello(request):
-    return render(request, 'index.html' )
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request,f'Welcome {username}.\nYour account has been created successfully.')
+            return redirect('login')
+    else:
+        form = RegisterForm()
+    
+    context = {'form': form}
+
+    return render(request, 'users/register.html', context)
