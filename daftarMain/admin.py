5ec129828889692f41eeec53778401e3fd0d3451
Soheil Khaki <soheilk81@gmail.com>
2024-08-03 15:25:19 +0330

    modified:   SaatDaftar/__pycache__/settings.cpython-311.pyc 	modified:   SaatDaftar/__pycache__/urls.cpython-311.pyc 	modified:   SaatDaftar/settings.py 	modified:   SaatDaftar/urls.py 	modified:   daftarMain/__pycache__/admin.cpython-311.pyc 	modified:   daftarMain/__pycache__/urls.cpython-311.pyc 	modified:   daftarMain/admin.py 	new file:   daftarMain/migrations/0003_delete_sallary_alter_officemanager_first_name_and_more.py 	new file:   daftarMain/migrations/__pycache__/0003_delete_sallary_alter_officemanager_first_name_and_more.cpython-311.pyc 	new file:   daftarMain/templates/registration/login.html 	modified:   daftarMain/urls.py 	modified:   db.sqlite3

--- daftarMain/admin.py
+++ daftarMain/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import OfficeManager, OfficeUser
 
-# Register your models here.
+@admin.register(OfficeManager)
+class OfficeManagerAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'phone', 'birth_date', 'code_meli')
+
+@admin.register(OfficeUser)
+class OfficeUserAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'phone', 'home_phone', 'birth_date', 'code_meli', 'office_admin')
