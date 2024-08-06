8d0a311853eea4ff675dc2c964300497eed87f72
Soheil Khaki <soheilk81@gmail.com>
2024-08-06 12:20:47 +0330

    modified:   daftarMain/__pycache__/views.cpython-311.pyc 	modified:   daftarMain/todo.txt 	modified:   daftarMain/views.py 	modified:   db.sqlite3

--- daftarMain/views.py
+++ daftarMain/views.py
@@ -29,16 +29,6 @@ def register_entry(request):
         return JsonResponse({"status": "success", "message": "Entry time registered."})
     return JsonResponse({"status": "error", "message": "Invalid request."}, status=400)
 
-# @login_required
-# def start_temporary_exit(request):
-#     if request.method == "POST":
-#         office_user = get_object_or_404(OfficeUser, user=request.user)
-#         clock_entry = Clock.objects.filter(office_user=office_user).latest('entry_to_office')
-#         clock_entry.wait_time_start = timezone.now()
-#         clock_entry.save()
-#         return JsonResponse({"status": "success", "message": "Temporary exit time started."})
-#     return JsonResponse({"status": "error", "message": "Invalid request."}, status=400)
-
 @login_required
 def register_exit(request):
     if request.method == "POST":
@@ -48,40 +38,6 @@ def register_exit(request):
         clock_entry.save()
         return JsonResponse({"status": "success", "message": "Exit time registered."})
     return JsonResponse({"status": "error", "message": "Invalid request."}, status=400)
-
-# @login_required
-# def office_manager_page(request):
-#     office_manager = get_object_or_404(OfficeManager, user=request.user)
-#     employees = OfficeUser.objects.filter(office_admin=office_manager)
-
-#     employee_statuses = []
-#     for employee in employees:
-#         # Check if the employee is in the office
-#         last_clock_entry = Clock.objects.filter(office_user=employee).order_by('-entry_to_office').first()
-#         is_in_office = last_clock_entry and not last_clock_entry.exit_from_office
-
-#         # Check if the employee has a pending or ongoing leave request
-#         now = timezone.now()
-#         has_leave_request = Leave.objects.filter(
-#             office_user=employee,
-#             leave_type='daily',
-#             start_date__lte=now,
-#             end_date__gte=now
-#         ).exists() or Leave.objects.filter(
-#             office_user=employee,
-#             leave_type='hourly',
-#             start_time__lte=now,
-#             end_time__gte=now
-#         ).exists()
-
-#         employee_statuses.append((employee, is_in_office, has_leave_request))
-
-#     context = {
-#         'office_manager': office_manager,
-#         'employees': employee_statuses,
-#     }
-    
-#     return render(request, 'office_manager_page.html', context)
 
 @login_required
 def office_manager_page(request):
@@ -116,7 +72,6 @@ def office_manager_page(request):
     }
     
     return render(request, 'office_manager_page.html', context)
-
 
 @login_required
 def add_office_user(request):
