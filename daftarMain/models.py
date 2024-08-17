4a80cfa92a6975ec061c2bda58b6b7c362c19192
Soheil Khaki <soheilk81@gmail.com>
2024-08-17 09:32:08 +0330

    modified:   daftarMain/__pycache__/forms.cpython-311.pyc 	modified:   daftarMain/__pycache__/models.cpython-311.pyc 	modified:   daftarMain/forms.py 	new file:   daftarMain/migrations/0019_officeuser_childeren_count_officeuser_code_bime_and_more.py 	new file:   daftarMain/migrations/__pycache__/0019_officeuser_childeren_count_officeuser_code_bime_and_more.cpython-311.pyc 	modified:   daftarMain/models.py 	modified:   db.sqlite3

--- daftarMain/models.py
+++ daftarMain/models.py
@@ -14,7 +14,14 @@ class OfficeManager(models.Model):
     def __str__(self):
         return f"{self.full_name}"
 
+
 class OfficeUser(models.Model):
+    MILITARY_SERVICE_TYPE = [
+        ('COM', 'Complete'),
+        ('PEN', 'Pending'),
+        ('N/A' , 'Do not have')
+    ]
+
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True) # remove null, blank = true
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
@@ -27,15 +34,21 @@ class OfficeUser(models.Model):
     staff_pic = models.ImageField(upload_to='staff_pics/', null=True, blank=True)
     address = models.TextField(null=True, blank=True)
     have_bime = models.BooleanField(default=False)
+    code_bime = models.CharField(max_length=30,null=True,blank=True)
+    family_phone_number = models.CharField(max_length=15,null=True,blank=True)
+    marital_status = models.BooleanField(default=False)
+    military_service_status = models.CharField(max_length=3 ,choices= MILITARY_SERVICE_TYPE, default='N/A')
+    childeren_count = models.CharField(max_length=3,default= 0)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+    
 
 class Clock(models.Model):
     entry_to_office = models.DateTimeField(null=True, blank=True)
     exit_from_office = models.DateTimeField(null=True, blank=True)
     is_reward_punishment = models.BooleanField(default=False) 
-    office_user = models.ForeignKey(OfficeUser, on_delete=models.CASCADE, null=True, blank=True) # remove null, blank = true
+    office_user = models.ForeignKey(OfficeUser, on_delete=models.PROTECT, null=True, blank=True) # remove null, blank = true
     projects = models.ManyToManyField('Project', related_name="clock_entries", blank=True)
 
     def __str__(self):
@@ -46,6 +59,7 @@ class Clock(models.Model):
             time_difference = self.exit_from_office - self.entry_to_office
             return time_difference.total_seconds() / 3600.0  # Convert seconds to hours
         return 0
+
 
 class Leave(models.Model):
     office_user = models.ForeignKey(OfficeUser, on_delete=models.CASCADE)
@@ -63,6 +77,7 @@ class Leave(models.Model):
     def __str__(self):
         return f"{self.office_user} - {self.leave_type} leave from {self.start_date or self.start_time} to {self.end_date or self.end_time}"
     
+
 class RegularRequest(models.Model):
     REQUEST_TYPES = [
         ('complaint', 'Complaint'),
@@ -78,6 +93,7 @@ class RegularRequest(models.Model):
     def __str__(self):
         return f"{self.get_request_type_display()} by {self.user.first_name} {self.user.last_name} on {self.submitted_at}"
 
+
 class Project(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
@@ -91,7 +107,7 @@ class Project(models.Model):
     def total_hours(self):
         return sum(log.hours_spent for log in self.projecttimelog_set.all())
 
-    
+
 class ProjectTimeLog(models.Model):
     office_user = models.ForeignKey(OfficeUser, on_delete=models.CASCADE)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
