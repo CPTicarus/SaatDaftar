7ad80ba04db6fedd5de3f903f191db4397c9d74f
Soheil Khaki <soheilk81@gmail.com>
2024-09-07 10:04:27 +0330

    modified:   SaatDaftar/__pycache__/settings.cpython-311.pyc 	modified:   SaatDaftar/settings.py 	modified:   daftarMain/__pycache__/admin.cpython-311.pyc 	modified:   daftarMain/admin.py 	new file:   daftarMain/templates/admin/jalali_change_form.html 	modified:   db.sqlite3 	new file:   staticfiles/admin/css/main.css 	new file:   staticfiles/admin/jquery.ui.datepicker.jalali/default.htm 	new file:   staticfiles/admin/jquery.ui.datepicker.jalali/scripts/calendar.all.js 	new file:   staticfiles/admin/jquery.ui.datepicker.jalali/scripts/calendar.js 	new file:   staticfiles/admin/jquery.ui.datepicker.jalali/scripts/jquery-1.10.2.min.js 	new file:   staticfiles/admin/jquery.ui.datepicker.jalali/scripts/jquery-1.10.2.min.map 	new file:   staticfiles/admin/jquery.ui.datepicker.jalali/scripts/jquery.ui.core.js 	new file:   staticfiles/admin/jquery.ui.datepicker.jalali/scripts/jquery.ui.datepicker-cc-fa.js 	new file:   staticfiles/admin/jquery.ui.datepicker.jalali/scripts/jquery.ui.datepicker-cc.js 	new file:   staticfiles/admin/jquery.ui.datepicker.jalali/themes/base/images/animated-overlay.gif 	new file:   staticfiles/admin/jquery.ui.datepicker.jalali/themes/base/images/icon-calendar.svg 	new file:   staticfiles/admin/jquery.ui.datepicker.jalali/themes/base/images/ui-bg_flat_30_cccccc_40x100.png 	new file:   staticfiles/admin/jquery.ui.datepicker.jalali/themes/base/images/ui-bg_flat_50_5c5c5c_40x100.png 	new file:   staticfiles/admin/jquery.ui.datepicker.jalali/themes/base/images/ui-bg_glass_20_555555_1x400.png 	new file:   staticfiles/admin/jquery.ui.datepicker.jalali/themes/base/images/ui-bg_glass_40_0078a3_1x400.png 	new file:   staticfiles/admin/jquery.ui.datepicker.jalali/themes/base/images/ui-bg_glass_40_ffc73d_1x400.png 	new file:   staticfiles/admin/jquery.ui.datepicker.jalali/themes/base/images/ui-bg_glass_65_ffffff_1x400.png 	new file:   staticfiles/admin/jquery.ui.datepicker.jalali/themes/base/images/ui-bg_glass_75_dadada_1x400.png 	new file:   staticfiles/admin/jquery.ui.datepicker.jalali/themes/base/images/ui-bg_glass_75_e6e6e6_1x400.png 	new file:   staticfiles/admin/jquery.ui.datepicker.jalali/themes/base/images/ui-bg_gloss-wave_25_333333_500x100.png 	new file:   staticfiles/admin/jquery.ui.datepicker.jalali/themes/base/images/ui-bg_highlight-soft_75_cccccc_1x100.png 	new file:   staticfiles/admin/jquery.ui.datepicker.jalali/themes/base/images/ui-bg_highlight-soft_80_eeeeee_1x100.png 	new file:   staticfiles/admin/jquery.ui.datepicker.jalali/themes/base/images/ui-bg_inset-soft_25_000000_1x100.png 	new file:   staticfiles/admin/jquery.ui.datepicker.jalali/themes/base/images/ui-bg_inset-soft_30_f58400_1x100.png 	new file:   staticfiles/admin/jquery.ui.datepicker.jalali/themes/base/images/ui-icons_222222_256x240.png 	new file:   staticfiles/admin/jquery.ui.datepicker.jalali/themes/base/images/ui-icons_454545_256x240.png 	new file:   staticfiles/admin/jquery.ui.datepicker.jalali/themes/base/images/ui-icons_4b8e0b_256x240.png 	new file:   staticfiles/admin/jquery.ui.datepicker.jalali/themes/base/images/ui-icons_a83300_256x240.png 	new file:   staticfiles/admin/jquery.ui.datepicker.jalali/themes/base/images/ui-icons_cccccc_256x240.png 	new file:   staticfiles/admin/jquery.ui.datepicker.jalali/themes/base/images/ui-icons_ffffff_256x240.png 	new file:   staticfiles/admin/jquery.ui.datepicker.jalali/themes/base/jquery-ui.css 	new file:   staticfiles/admin/jquery.ui.datepicker.jalali/themes/base/jquery-ui.min.css 	new file:   staticfiles/admin/jquery.ui.datepicker.jalali/themes/base/jquery.ui.theme.css 	new file:   staticfiles/admin/main.js 	modified:   staticfiles/daftarMain/css/staff_management.css

--- SaatDaftar/settings.py
+++ SaatDaftar/settings.py
@@ -120,11 +120,11 @@ JALALI_DATE_DEFAULTS = {
             # loading datepicker
             'admin/js/django_jalali.min.js',
             # OR
-            # 'admin/jquery.ui.datepicker.jalali/scripts/jquery.ui.core.js',
-            # 'admin/jquery.ui.datepicker.jalali/scripts/calendar.js',
-            # 'admin/jquery.ui.datepicker.jalali/scripts/jquery.ui.datepicker-cc.js',
-            # 'admin/jquery.ui.datepicker.jalali/scripts/jquery.ui.datepicker-cc-fa.js',
-            # 'admin/js/main.js',
+            'admin/jquery.ui.datepicker.jalali/scripts/jquery.ui.core.js',
+            'admin/jquery.ui.datepicker.jalali/scripts/calendar.js',
+            'admin/jquery.ui.datepicker.jalali/scripts/jquery.ui.datepicker-cc.js',
+            'admin/jquery.ui.datepicker.jalali/scripts/jquery.ui.datepicker-cc-fa.js',
+            'admin/js/main.js',
         ],
         'css': {
             'all': [
--- daftarMain/admin.py
+++ daftarMain/admin.py
@@ -1,10 +1,26 @@
 from django.contrib import admin
 from .models import OfficeManager, OfficeUser
+from jalali_date import datetime2jalali, date2jalali
+from jalali_date.admin import ModelAdminJalaliMixin
 
 @admin.register(OfficeManager)
-class OfficeManagerAdmin(admin.ModelAdmin):
-    list_display = ('full_name', 'email', 'phone', 'birth_date', 'code_meli')
+class OfficeManagerAdmin(ModelAdminJalaliMixin, admin.ModelAdmin):
+    list_display = ('full_name', 'email', 'phone', 'get_birth_date_jalali', 'code_meli')
+
+    # Convert birth_date to Jalali in the admin display
+    @admin.display(description='تاریخ تولد')
+    def get_birth_date_jalali(self, obj):
+        if obj.birth_date:
+            return date2jalali(obj.birth_date).strftime('%Y/%m/%d')
+        return '-'
 
 @admin.register(OfficeUser)
-class OfficeUserAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'phone', 'home_phone', 'birth_date', 'code_meli', 'office_admin')
+class OfficeUserAdmin(ModelAdminJalaliMixin, admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'phone', 'home_phone', 'get_birth_date_jalali', 'code_meli', 'office_admin')
+
+    # Convert birth_date to Jalali in the admin display
+    @admin.display(description='تاریخ تولد')
+    def get_birth_date_jalali(self, obj):
+        if obj.birth_date:
+            return date2jalali(obj.birth_date).strftime('%Y/%m/%d')
+        return '-'
